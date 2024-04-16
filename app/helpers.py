5af88eb9faf100ac12d1639606fcc0b7ebff360d
Akbar Ali <akbar@intechinnovates.com>
2024-04-16 23:35:22 +0400

    bugfix: amount shown was not correct

--- app/helpers.py
+++ app/helpers.py
@@ -1,34 +1,41 @@
 from models import OrderBook
 from db import get_session
 
-def get_price_to_buy_10_btc(session=get_session()):
-    """Calculate and return the price to buy 10 bitcoin."""
+def get_price_to_buy_btc(required_qty=10,session=get_session()):
+    """Calculate and return the price to buy bitcoin."""
     asks = session.query(OrderBook).filter(
         OrderBook.order_type == 'ask',
-        OrderBook.quantity <= 10,
-        ).order_by(OrderBook.rate.asc()).all()
+        OrderBook.quantity <= required_qty,
+        ).order_by(OrderBook.rate.asc(),OrderBook.quantity.asc()).all()
 
     cumulative_quantity = 0.0
+    cumulative_amount = 0.0
 
     for ask in asks:
+        # print(ask)
         cumulative_quantity += ask.quantity
-        if cumulative_quantity >= 10:
-            return ask.amount
+        cumulative_amount += ask.amount
+        if cumulative_quantity >= required_qty:
+            return cumulative_amount, cumulative_quantity
 
     return None
 
 
-def get_price_to_sell_10_btc(session=get_session()):
-    """Calculate and return the price to sell 10 bitcoin."""
+def get_price_to_sell_btc(required_qty=10,session=get_session()):
+    """Calculate and return the price to sell bitcoin."""
     bids = session.query(OrderBook).filter(
         OrderBook.order_type == 'bid',
-        OrderBook.quantity <= 10,
-        ).order_by(OrderBook.rate.desc()).all()
+        OrderBook.quantity <= required_qty,
+        ).order_by(OrderBook.rate.desc(), OrderBook.quantity.asc()).all()
 
     cumulative_quantity = 0.0
+    cumulative_amount = 0.0
 
     for bid in bids:
+        # print(bid)
         cumulative_quantity += bid.quantity
-        if cumulative_quantity >= 10:
-            return bid.amount
+        cumulative_amount += bid.amount
+
+        if cumulative_quantity >= required_qty:
+            return cumulative_amount, cumulative_quantity
     return None
